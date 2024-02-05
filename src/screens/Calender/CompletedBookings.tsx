a4ec617806948f941b62a05a36389ead37fe6208
Ranvijay <ranvijaychouhan12@gmail.com>
2024-02-05 17:23:54 +0530

    Booking Page Empty Element Handled

--- src/screens/Calender/CompletedBookings.tsx
+++ src/screens/Calender/CompletedBookings.tsx
@@ -1,9 +1,20 @@
-import { StyleSheet, View, FlatList, RefreshControl } from "react-native";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  RefreshControl,
+  Dimensions,
+} from "react-native";
 import React, { useCallback, useRef, useState } from "react";
-import { useAppDispatch, useAppSelector } from "@src/redux/store";
+import { useAppDispatch, useAppSelector } from "@redux/store";
 import BookingCard from "@cards/Booking/BookingCard";
-import Utils from "@src/common/Utils";
+import Utils from "@common/Utils";
 import { loadBooking } from "@reducers/AppDataSlice";
+import svgs from "@common/AllSvgs";
+import AppText from "@components/Text/AppText";
+import { VerticalSpacing } from "@components/Spacing/Spacing";
+
+const windowHeight = Dimensions.get("window").height;
 
 export default function CompletedBookings(props: any) {
   const { theme } = useAppSelector(state => state.theme);
@@ -21,35 +32,48 @@ export default function CompletedBookings(props: any) {
   }, []);
 
   return (
-    <View style={{}}>
-      <FlatList
-        ref={scrollUpBookings}
-        contentContainerStyle={{
-          paddingTop: 15,
-          paddingBottom: 100,
-          rowGap: 10,
-          marginHorizontal: 15,
-        }}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-        showsVerticalScrollIndicator={false}
-        data={completedBookings}
-        renderItem={({ item, index }) => (
-          <BookingCard
-            key={index}
-            startTime={item?.startTime}
-            endTime={item?.endTime}
-            bookingStatusType={item?.bookingStatusType}
-            bookingType={item?.bookingType}
-            bookingNumber={item?.bookingNumber}
-            coachSessionTypeID={item?.coachSessionTypeID}
-            court={item?.court}
-            coach={item?.coach}
-            location={item?.location}
-          />
-        )}
-      />
+    <View style={{ flex: 1 }}>
+      {completedBookings && CompletedBookings.length > 0 ? (
+        <FlatList
+          ref={scrollUpBookings}
+          contentContainerStyle={{
+            paddingTop: 15,
+            paddingBottom: 100,
+            rowGap: 10,
+            marginHorizontal: 15,
+          }}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+          showsVerticalScrollIndicator={false}
+          data={completedBookings}
+          renderItem={({ item, index }) => (
+            <BookingCard
+              key={index}
+              startTime={item?.startTime}
+              endTime={item?.endTime}
+              bookingStatusType={item?.bookingStatusType}
+              bookingType={item?.bookingType}
+              bookingNumber={item?.bookingNumber}
+              coachSessionTypeID={item?.coachSessionTypeID}
+              court={item?.court}
+              coach={item?.coach}
+              location={item?.location}
+            />
+          )}
+        />
+      ) : (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <svgs.NoBooking height={windowHeight / 2.5} width="80%" />
+          <VerticalSpacing />
+          <AppText fontStyle="500.bold" size={16}>
+            Booking History
+          </AppText>
+          <VerticalSpacing />
+          <AppText>Track your ongoing and completed bookings here</AppText>
+        </View>
+      )}
     </View>
   );
 }
--- src/screens/Calender/UpcomingBookings.tsx
+++ src/screens/Calender/UpcomingBookings.tsx
@@ -1,9 +1,20 @@
-import { StyleSheet, View, FlatList, RefreshControl } from "react-native";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  RefreshControl,
+  Dimensions,
+} from "react-native";
 import React, { useCallback, useRef, useState } from "react";
-import { useAppDispatch, useAppSelector } from "@src/redux/store";
+import { useAppDispatch, useAppSelector } from "@redux/store";
 import BookingCard from "@cards/Booking/BookingCard";
-import Utils from "@src/common/Utils";
+import Utils from "@common/Utils";
 import { loadBooking } from "@reducers/AppDataSlice";
+import svgs from "@common/AllSvgs";
+import AppText from "@src/components/Text/AppText";
+import { VerticalSpacing } from "@src/components/Spacing/Spacing";
+
+const windowHeight = Dimensions.get("window").height;
 
 export default function UpcomingBookings(props: any) {
   const { theme } = useAppSelector(state => state.theme);
@@ -21,35 +32,48 @@ export default function UpcomingBookings(props: any) {
   }, []);
 
   return (
-    <View style={{}}>
-      <FlatList
-        ref={scrollUpBookings}
-        contentContainerStyle={{
-          paddingTop: 15,
-          paddingBottom: 100,
-          rowGap: 10,
-          marginHorizontal: 15,
-        }}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-        }
-        showsVerticalScrollIndicator={false}
-        data={upComingBookings}
-        renderItem={({ item, index }) => (
-          <BookingCard
-            key={index}
-            startTime={item?.startTime}
-            endTime={item?.endTime}
-            bookingStatusType={item?.bookingStatusType}
-            bookingType={item?.bookingType}
-            bookingNumber={item?.bookingNumber}
-            coachSessionTypeID={item?.coachSessionTypeID}
-            court={item?.court}
-            coach={item?.coach}
-            location={item?.location}
-          />
-        )}
-      />
+    <View style={{ flex: 1 }}>
+      {upComingBookings && upComingBookings?.length > 0 ? (
+        <FlatList
+          ref={scrollUpBookings}
+          contentContainerStyle={{
+            paddingTop: 15,
+            paddingBottom: 100,
+            rowGap: 10,
+            marginHorizontal: 15,
+          }}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+          showsVerticalScrollIndicator={false}
+          data={upComingBookings}
+          renderItem={({ item, index }) => (
+            <BookingCard
+              key={index}
+              startTime={item?.startTime}
+              endTime={item?.endTime}
+              bookingStatusType={item?.bookingStatusType}
+              bookingType={item?.bookingType}
+              bookingNumber={item?.bookingNumber}
+              coachSessionTypeID={item?.coachSessionTypeID}
+              court={item?.court}
+              coach={item?.coach}
+              location={item?.location}
+            />
+          )}
+        />
+      ) : (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <svgs.NoBooking height={windowHeight / 2.5} width="80%" />
+          <VerticalSpacing />
+          <AppText fontStyle="500.bold" size={16}>
+            You haven’t made a booking yet
+          </AppText>
+          <VerticalSpacing />
+          <AppText>Once you make a booking, it will appear here.</AppText>
+        </View>
+      )}
     </View>
   );
 }
